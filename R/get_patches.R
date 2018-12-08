19b1f2a795342008913edf26f5be8cccfe080520
jn <tupiszakaczki@gmail.com>
2018-12-08 21:18:31 +0100

    uses simpler code for n=4

--- R/get_patches.R
+++ R/get_patches.R
@@ -1,41 +1,58 @@
-get_patches <- function(landscape) UseMethod("get_patches")
+get_patches <- function(landscape, class, direction = 8) UseMethod("get_patches")
 
 get_patches.sf <- function(landscape, class, direction = 8){
 
-  lsc_classes <- unique(dplyr::pull(landscape, !!class))
+  landscape_cast <- landscape %>%
+    sf::st_cast("POLYGON", warn = FALSE)
 
-  if (direction == 4) nb_string =  "F***1****"
-  if (direction == 8) nb_string =  "F***T****"
+  if (direction == 4) {
+    class <- rlang::enquo(class)
+    result <- landscape_cast %>%
+      dplyr::group_by(!!class) %>%
+      dplyr::mutate(patch = seq_len(dplyr::n()))
+    result$patch = as.factor(result$patch)
+    return(result)
 
-  landscape_cast <- sf::st_cast(landscape, "POLYGON", warn = FALSE)
+  } else if (direction == 8){
+    nb_string <- "F***T****"
+    lsc_classes <- unique(dplyr::pull(landscape, !!class))
 
-  landscape_nb <- purrr::map(lsc_classes, function(class_i) {
-    a <- dplyr::filter(landscape_cast, class == class_i)
+    landscape_nb <- purrr::map(lsc_classes, function(class_i) {
+      a <- dplyr::filter(landscape_cast, class == class_i)
 
-    nb <- sf::st_relate(a, a, pattern = nb_string, sparse = FALSE)
-    nb[lower.tri(nb)] <- NA
+      nb <- sf::st_relate(a, a, pattern = nb_string, sparse = FALSE)
+      nb[lower.tri(nb)] <- NA
 
-    landscape_nb <- purrr::map(seq_len(ncol(nb)), .f = function(patch_i, class_i) {
-      nb_ind <- which(nb[, patch_i] == TRUE)
-      nb_union <- sf::st_union(a[c(nb_ind, patch_i), ])
-      nb_union <- sf::st_sf(geometry = nb_union)
-      nb_union$class <- class_i
-      nb_union$patch <- patch_i
-      nb_union
-    }, class_i)
+      landscape_nb <- purrr::map(seq_len(ncol(nb)), .f = function(patch_i, class_i) {
+        nb_ind <- which(nb[, patch_i] == TRUE)
+        nb_union <- sf::st_union(a[c(nb_ind, patch_i), ])
+        nb_union <- sf::st_sf(geometry = nb_union)
+        nb_union$class <- class_i
+        nb_union$patch <- patch_i
+        nb_union
+      }, class_i)
 
-    # for(class_i in seq_len(ncol(nb))) {
-    #
-    #   nb_ind <- which(nb[, class_i] == TRUE)
-    #   if(length(nb_ind) != 0) landscape_nb[nb_ind] <- NULL
-    #
-    # }
-    landscape_nb
-  })
-  landscape_nb = purrr::flatten(landscape_nb)
+      # for(class_i in seq_len(ncol(nb))) {
+      #
+      #   nb_ind <- which(nb[, class_i] == TRUE)
+      #   if(length(nb_ind) != 0) landscape_nb[nb_ind] <- NULL
+      #
+      # }
+      landscape_nb
+    })
+    landscape_nb = purrr::flatten(landscape_nb)
 
-  result <- do.call(rbind, landscape_nb)
-  result <- sf::st_collection_extract(result, "POLYGON")
-  result
+    result <- do.call(rbind, landscape_nb)
+    result <- sf::st_collection_extract(result, "POLYGON")
+    return(result)
+  }
 }
 
+# r4 = get_patches(vector_landscape, class, direction = 4)
+# r8 = get_patches(vector_landscape, class)
+#
+# r4$patch = as.factor(r4$patch)
+# r8$patch = as.factor(r8$patch)
+#
+# plot(r4[2])
+# plot(r8[2])
