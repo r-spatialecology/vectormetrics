621bf066728242d23ae4ae4080b5c33ea0d9a06d
jn <tupiszakaczki@gmail.com>
2019-03-30 15:44:00 +0100

    adds documentation

--- R/cv.R
+++ R/cv.R
@@ -16,14 +16,11 @@ vm_cv <- function(x){
   x_mean <- mean(x)
   x_sd   <- sd(x)
 
-
   cv <- (x_sd / x_mean) * 100
 
   return(cv)
 
 }
 
-
-aggregate(area$value, by= list(area$class), raster::cv)
-
-aggregate(area$value, by= list(area$class), vm_cv)
+# aggregate(area$value, by= list(area$class), raster::cv)
+# aggregate(area$value, by= list(area$class), vm_cv)
