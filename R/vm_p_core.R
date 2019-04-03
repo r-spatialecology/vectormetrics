c364a3439f8e7c88093e63be76cdeddc88c3c772
marco <sciaini.marco@gmail.com>
2019-04-03 09:42:04 +0200

    :recycle: refactor core on patch level, should be OK now

--- R/vm_p_core.R
+++ R/vm_p_core.R
@@ -13,13 +13,15 @@
 #' vm_p_core(vector_landscape, "class", edge_depth = 0.8)
 #' @export
 
-# core
 vm_p_core <- function(landscape, class, edge_depth) {
 
   # check whether the input is a MULTIPOLYGON or a POLYGON
   if(!all(sf::st_geometry_type(landscape) %in% c("MULTIPOLYGON", "POLYGON"))){
     stop("Please provide PLOYGON or MULTIPLOYGON")
   }
+
+  # select geometry column for spatial operations and the column that identifies the classes
+  landscape <- landscape[, c(class, "geometry")]
 
   # extract the multipolygon, cast to single polygons (patch level)
   landscape <- sf::st_cast(landscape, "POLYGON", warn = FALSE)
@@ -30,11 +32,13 @@ vm_p_core <- function(landscape, class, edge_depth) {
   # calculate the area of each core area
   landscape$core <- sf::st_area(core_area)/10000
 
-  # return results tibble
-  class_ids <- dplyr::pull(sf::st_set_geometry(landscape, NULL), class)
+  # get class ids and if factor, coerce to numeric
+  class_ids <-  sf::st_set_geometry(landscape, NULL)[, class]
   if (class(class_ids) == "factor"){
     class_ids <- as.numeric(levels(class_ids))[class_ids]
   }
+
+  # return result tibble
   tibble::tibble(
     level = "patch",
     class = as.integer(class_ids),
