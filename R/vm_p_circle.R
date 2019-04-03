9feb693001c5ac37bf1ebfcf64fc280bb9498be3
marco <sciaini.marco@gmail.com>
2019-04-03 11:51:01 +0200

    :recycle: finish vm_p_circle

--- R/vm_p_circle.R
+++ R/vm_p_circle.R
@@ -1,58 +1,97 @@
-
-#' @title Ratio between the patch area and the smallest circumscribing circle of the patch.
+#' CIRCLE (patch level)
 #'
-#' @description This function allows you to calculate the ratio between the patch area and the smallest circumscribing circle of the patch.
-#' The diameter of the smallest circumscribing circle is the The distance between the two farthest points on the patch.
-#' @param landscape the input landscape image,
-#' @param class the name of the class column of the input landscape
-#' @return  the function returns the calculated ratio of all patches in column "value",
-#' and this function returns also some important information such as level, class, patch id and metric name.
-#' @examples
-#' ## if the class name of input landscape is landcover,
-#' ## then write landcover in a double quotation marks as the second parameter.
+#' @description Related Circumscribing Circle (Shape metric)
+#'
+#' @param landscape *sf* MULTIPOLYGON or POLYGON feature
+#' @param class Name of the class column of the input landscape
+#'
+#' @details
+#' \deqn{CIRCLE = 1 - (\frac{a_{ij}} {a_{ij}^{circle}})}
+#' where \eqn{a_{ij}} is the area in square meters and \eqn{a_{ij}^{circle}} the area of
+#' the smallest circumscribing circle.
+#'
+#' CIRCLE is a 'Shape metric'. The metric is the ratio between the patch area and the smallest
+#' circumscribing circle of the patch. The diameter of the smallest circumscribing circle is
+#' the 'diameter' of the patch connecting the opposing corner points of the polygon
+#' that are the furthest away from each other. The metric characterises the compactness
+#' of the patch and is comparable among patches with different area.
+#'
+#' \subsection{Units}{None}
+#' \subsection{Range}{0 <= CIRCLE < 1}
+#' \subsection{Behaviour}{CIRCLE = 0 for a circular patch and approaches CIRCLE = 1 for
+#' a linear patch.}
+#'
+#' @examples-
 #' vm_p_circle(vector_landscape, "class")
+#'
+#' @aliases vm_p_circle
+#' @rdname vm_p_circle
+#'
+#' @references
+#' McGarigal, K., SA Cushman, and E Ene. 2012. FRAGSTATS v4: Spatial Pattern Analysis
+#' Program for Categorical and Continuous Maps. Computer software program produced by
+#' the authors at the University of Massachusetts, Amherst. Available at the following
+#' web site: http://www.umass.edu/landeco/research/fragstats/fragstats.html
+#'
+#' Baker, W. L., and Y. Cai. 1992. The r.le programs for multiscale analysis of
+#' landscape structure using the GRASS geographical information system.
+#' Landscape Ecology 7: 291-302.
+#'
 #' @export
+vm_p_circle <- function(landscape, class) UseMethod("vm_p_circle")
 
-
-vm_p_circle <- function(landscape, class) {
+#' @name vm_p_circle
+#' @export
+vm_p_circle.sf <- function(landscape, class) {
 
   # check whether the input is a MULTIPOLYGON or a POLYGON
   if(!all(sf::st_geometry_type(landscape) %in% c("MULTIPOLYGON", "POLYGON"))){
     stop("Please provide POLYGON or MULTIPOLYGON simple feature.")
   }
 
-  # select geometry column for spatial operations and the column that identifies
-  # the classes
+  # select geometry column for spatial operations and the column that identifies the classes
   landscape <- landscape[, c(class, "geometry")]
 
   # extract the multipolygon, cast to single polygons (patch level)
   landscape <- sf::st_cast(landscape, "POLYGON", warn = FALSE)
 
-  # cast then to MULTILINESTRING
+  # cast then to MULTIPOINT
   landscape_cast <- sf::st_cast(landscape, "MULTIPOINT", warn = FALSE, do_split = FALSE)
 
-  # create empty variable to be filled
-  dis_max <- vector("double", length = nrow(landscape_cast))
-
-  for (i in seq_len(nrow(landscape_cast))) {
+  # compute max distant for each MULTIPOINT, which is the diameter of a circle around each patch
+  dis_max <- sapply(seq_along(dis_max), function(i){
     landscape_point <- sf::st_cast(landscape_cast[i, ], "POINT", warn = FALSE)
     dis <- sf::st_distance(landscape_point, by_element = F)
-    dis_max[i] <- max(dis)
-  }
+    max(dis)
+  })
 
+  # calculate circle metric
   circle_area <- vm_p_area(landscape, class)
   circum_area <- pi * (dis_max / 2)^2
   landscape_cast$circle <- 1 - (circle_area$value*10000/ circum_area)
 
-  # return result tibble
-  class_ids <- sf::st_set_geometry(landscape, NULL)
+  # get class ids and if factor, coerce to numeric
+  class_ids <-  sf::st_set_geometry(landscape, NULL)[, class]
+  if (class(class_ids) == "factor"){
+    class_ids <- as.numeric(levels(class_ids))[class_ids]
+  }
 
+  # return result tibble
   tibble::tibble(
     level = "patch",
-    class = as.integer(class_ids[, 1]),
+    class = as.integer(class_ids),
     id = as.integer(1:nrow(landscape_cast)),
     metric = "circle",
     value = as.double(landscape_cast$circle)
   )
+
+}
+
+#' @name vm_p_circle
+#' @export
+vm_p_circle.SpatialPolygonsDataFrame <- function(landscape, class) {
+
+  vm_p_circle <- sf::st_as_sf(landscape)
+  vm_p_circle(landscape, class, edge_depth)
 
 }
