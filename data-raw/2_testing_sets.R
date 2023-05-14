de84dd53ac75f8a09c6f04dfa2ecb5430ade2b8d
Tomek Matuszek <tom.mateuszek@gmail.com>
2023-05-15 00:24:31 +0200

    update testing datasets

--- data-raw/2_testing_sets.R
+++ data-raw/2_testing_sets.R
@@ -1,8 +1,11 @@
-poz_buldings <- sf::st_read("./testing_sets/buildings.gpkg")
-usethis::use_data(poz_buldings, overwrite = TRUE)
+poz_buildings <- sf::st_read("./testing_sets/buildings.gpkg")
+sf::st_geometry(poz_buildings) <- "geometry"
+usethis::use_data(poz_buildings, overwrite = TRUE)
 
 clc <- sf::st_read("./testing_sets/clc.gpkg")
+sf::st_geometry(clc) <- "geometry"
 usethis::use_data(clc, overwrite = TRUE)
 
 urban_atlas <- sf::st_read("./testing_sets/urbanatlas.gpkg")
+sf::st_geometry(urban_atlas) <- "geometry"
 usethis::use_data(urban_atlas, overwrite = TRUE)
