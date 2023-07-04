1a911010ba784cc19f621dd5fa9d6a7224420b92
Tomek Matuszek <tom.mateuszek@gmail.com>
2023-07-04 19:23:46 +0200

    form factor/compactness

---
+++ R/vm_p_comp_idx.R
@@ -0,0 +1,45 @@
+#' @title Form factor/Compactness(vector data)
+#'
+#' @description Calculate form factor or compactness
+#' @param landscape the input landscape image,
+#' @param class the name of the class column of the input landscape
+#' @return sqrt(4 * area / pi) / perimeter
+#' ## if the class name of input landscape is landcover,
+#' ## then write landcover in a double quotation marks as the second parameter.
+#' vm_p_comp_idx(vector_landscape, "class")
+#' @export
+
+vm_p_comp_idx <- function(landscape, class) {
+  # check whether the input is a MULTIPOLYGON or a POLYGON
+  if(!all(sf::st_geometry_type(landscape) %in% c("MULTIPOLYGON", "POLYGON"))){
+    stop("Please provide POLYGON or MULTIPOLYGON")
+  } else if (all(sf::st_geometry_type(landscape) == "MULTIPOLYGON")){
+    message("MULTIPOLYGON geometry provided. You may want to cast it to seperate polygons with 'get_patches()'.")
+  }
+
+  # select geometry column for spatial operations and the column that identifies the classes
+  landscape <- landscape[, class]
+
+  # calculate the area of polygons
+  landscape$area <- vm_p_area(landscape, class)$value * 10000
+
+  # calculate the perimeter of polygons
+  landscape$perim <- vm_p_perim(landscape, class)$value
+
+  # ratio of perimeter of convex hull and polygon perimeters
+  comp_index <- (4 * pi * landscape$area) / landscape$perim ^ 2
+
+  # return results tibble
+  class_ids <- sf::st_set_geometry(landscape, NULL)[, class, drop = TRUE]
+  if (is(class_ids, "factor")){
+    class_ids <- as.numeric(levels(class_ids))[class_ids]
+  }
+
+  tibble::tibble(
+    level = "patch",
+    class = as.integer(class_ids),
+    id = as.integer(1:nrow(landscape)),
+    metric = "comp_index",
+    value = as.double(comp_index)
+  )
+}
