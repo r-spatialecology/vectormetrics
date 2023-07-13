b83efb4f31ef881562575b349d9a5042abeb0026
Tomek Matuszek <tom.mateuszek@gmail.com>
2023-07-13 19:36:16 +0200

    added circularity

---
+++ R/vm_p_circ_idx.R
@@ -0,0 +1,46 @@
+#' @title Circularity(vector data)
+#'
+#' @description Circularity convexity
+#' @param landscape the input landscape image,
+#' @param class the name of the class column of the input landscape
+#' @return  ratio between area of polygon and area of equal-perimeter circle
+#' ## if the class name of input landscape is landcover,
+#' ## then write landcover in a double quotation marks as the second parameter.
+#' vm_p_circ_idx(vector_landscape, "class")
+#' @export
+
+vm_p_circ_idx <- function(landscape, class) {
+  # check whether the input is a MULTIPOLYGON or a POLYGON
+  if(!all(sf::st_geometry_type(landscape) %in% c("MULTIPOLYGON", "POLYGON"))){
+    stop("Please provide POLYGON or MULTIPOLYGON")
+  } else if (all(sf::st_geometry_type(landscape) == "MULTIPOLYGON")){
+    message("MULTIPOLYGON geometry provided. You may want to cast it to seperate polygons with 'get_patches()'.")
+  }
+
+  # select geometry column for spatial operations and the column that identifies the classes
+  landscape <- landscape[, class]
+
+  # calculate the perimeter of polygons
+  landscape$area <- vm_p_area(landscape, class)$value * 10000
+
+  # calculate the perimeter of convex hull
+  perim <- vm_p_perim(landscape, class)$value
+  landscape$epc_area <- (perim / (2 * pi)) ^ 2 * pi
+
+  # ratio of perimeter of convex hull and polygon perimeters
+  circ_index <- landscape$area / landscape$epc_area
+
+  # return results tibble
+  class_ids <- sf::st_set_geometry(landscape, NULL)[, class, drop = TRUE]
+  if (is(class_ids, "factor")){
+    class_ids <- as.numeric(levels(class_ids))[class_ids]
+  }
+
+  tibble::tibble(
+    level = "patch",
+    class = as.integer(class_ids),
+    id = as.integer(1:nrow(landscape)),
+    metric = "circ_index",
+    value = as.double(circ_index)
+  )
+}
