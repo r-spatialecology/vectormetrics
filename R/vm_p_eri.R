a647251d455f476f29f241045cef417ae4519e7e
Tomek Matuszek <tom.mateuszek@gmail.com>
2023-11-05 23:16:19 +0100

    equivalent rectangular index and roughness index

---
+++ R/vm_p_eri.R
@@ -0,0 +1,44 @@
+#' @title Equivalent rectangular index(vector data)
+#'
+#' @description Calculate Equivalent rectangular index (ERI)
+#' @param landscape the input landscape image,
+#' @param class the name of the class column of the input landscape
+#' @return  ratio between perimeter of equal-area rectangle of shape and perimeter of shape
+#' ## if the class name of input landscape is landcover,
+#' ## then write landcover in a double quotation marks as the second parameter.
+#' vm_p_sq_idx(vector_landscape, "class")
+#' @export
+
+vm_p_eri <- function(landscape, class){
+  # check whether the input is a MULTIPOLYGON or a POLYGON
+  if(!all(sf::st_geometry_type(landscape) %in% c("MULTIPOLYGON", "POLYGON"))){
+    stop("Please provide POLYGON or MULTIPOLYGON")
+  } else if (all(sf::st_geometry_type(landscape) == "MULTIPOLYGON")){
+    message("MULTIPOLYGON geometry provided. You may want to cast it to seperate polygons with 'get_patches()'.")
+  }
+
+  # select geometry column for spatial operations and the column that identifies the classes
+  landscape <- landscape[, class]
+
+  perim <- vm_p_perim(landscape, class)$value
+  area <- vm_p_area(landscape, class)$value * 10000
+  mabr_area <- landscape |> sf::st_minimum_rotated_rectangle() |> vm_p_area(class)
+  mabr_perim <- landscape |> sf::st_minimum_rotated_rectangle() |> vm_p_perim(class)
+
+  ear_perim <- sqrt(area / (mabr_area$value * 10000)) * mabr_perim$value
+  eri <- ear_perim / perim
+
+  # return results tibble
+  class_ids <- sf::st_set_geometry(landscape, NULL)[, class, drop = TRUE]
+  if (is(class_ids, "factor")){
+    class_ids <- as.numeric(levels(class_ids))[class_ids]
+  }
+
+  tibble::tibble(
+    level = "patch",
+    class = as.integer(class_ids),
+    id = as.integer(1:nrow(landscape)),
+    metric = "ERI",
+    value = as.double(eri)
+  )
+}
