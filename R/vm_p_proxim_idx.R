e00f0f4b317844c2f65cf7cb8676d7aed524af23
Tomek <tom.mateuszek@gmail.com>
2023-12-11 10:41:07 +0100

    proximity index

---
+++ R/vm_p_proxim_idx.R
@@ -0,0 +1,51 @@
+#' @title Proximity Index(vector data)
+#'
+#' @description Calculate Proximity Index
+#' @param landscape the input landscape image,
+#' @param class the name of the class column of the input landscape
+#' @return  ratio between average distance from all points of equal-area circle to its center and average distance from all points of shape to its center
+#' ## if the class name of input landscape is landcover,
+#' ## then write landcover in a double quotation marks as the second parameter.
+#' vm_p_proxim_idx(vector_landscape, "class")
+#' @export
+
+vm_p_proxim_idx <- function(landscape, class, n = 1000) {
+  # check whether the input is a MULTIPOLYGON or a POLYGON
+  if(!all(sf::st_geometry_type(landscape) %in% c("MULTIPOLYGON", "POLYGON"))){
+    stop("Please provide POLYGON or MULTIPOLYGON")
+  } else if (all(sf::st_geometry_type(landscape) == "MULTIPOLYGON")){
+    message("MULTIPOLYGON geometry provided. You may want to cast it to seperate polygons with 'get_patches()'.")
+  }
+
+  # select geometry column for spatial operations and the column that identifies the classes
+  landscape <- landscape[, class]
+
+  progress_bar <- txtProgressBar(min = 0, max = nrow(landscape), style = 3, char = "=")
+  for (i in 1:nrow(landscape)){
+    shape <- landscape[i, ]
+    igp <- get_igp(shape, n)
+    cent <- geos::geos_centroid(shape)
+
+    igp_dist <- geos::geos_distance(igp, cent)
+    landscape$igp_dist[i] <- mean(igp_dist)
+    setTxtProgressBar(progress_bar, value = i)
+  }
+  close(progress_bar)
+
+  radiuses = vm_p_circlep(landscape, class)$value / (2 * pi) * 0.66
+  proximity = radiuses / landscape$igp_dist
+
+  # return results tibble
+  class_ids <- sf::st_set_geometry(landscape, NULL)[, class, drop = TRUE]
+  if (is(class_ids, "factor")){
+    class_ids <- as.numeric(levels(class_ids))[class_ids]
+  }
+
+  tibble::tibble(
+    level = "patch",
+    class = as.integer(class_ids),
+    id = as.integer(1:nrow(landscape)),
+    metric = "proximity_index",
+    value = as.double(proximity)
+  )
+}
