d967fa80264d264889e2caed6f789189fb7623ce
Tomek Matuszek <tom.mateuszek@gmail.com>
2024-01-13 12:20:27 +0100

    profiling

---
+++ profiling.R
@@ -0,0 +1,10 @@
+library(vectormetrics)
+devtools::load_all()
+
+vl = get_patches(vector_landscape, "class")
+Rprof("profile.out")
+vm_p_exchange_idx(poz_buildings[1:100,], "building")
+Rprof(NULL)
+summaryRprof("profile.out")
+
+system.time(vm_p_range_idx(poz_buildings, "building"))
