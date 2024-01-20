64973a9324e38d4ecc4b82af1cbd3a7ae1c04480
Tomek Matuszek <tom.mateuszek@gmail.com>
2024-01-20 19:15:40 +0100

    added testing shape with txt class

--- data-raw/3_testing_shapes.R
+++ data-raw/3_testing_shapes.R
@@ -13,6 +13,12 @@ circle <- sf::st_point(c(0, 0)) |> sf::st_buffer(1) |>
 sf::st_geometry(circle) <- "geometry"
 circle$class <- 1
 
+squaretxt <- list(cbind(c(0,1,1,0,0), c(0,0,1,1,0))) |> sf::st_polygon() |>
+  sf::st_sfc() |> sf::st_sf(crs = "EPSG:2180")
+sf::st_geometry(squaretxt) <- "geometry"
+squaretxt$class <- "text"
+
 usethis::use_data(square, overwrite = TRUE)
 usethis::use_data(diamond, overwrite = TRUE)
 usethis::use_data(circle, overwrite = TRUE)
+usethis::use_data(squaretxt, overwrite = TRUE)
