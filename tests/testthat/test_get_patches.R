93a03772b847bd651c70358b7f057a9df94f61da
Tomek Matuszek <tom.mateuszek@gmail.com>
2024-02-01 23:54:29 +0100

    checking column names in get_patches unit test

--- tests/testthat/test_get_patches.R
+++ tests/testthat/test_get_patches.R
@@ -4,7 +4,11 @@ testthat::test_that("check number of patches", {
 })
 
 testthat::test_that("check get_patches result structure", {
+  vl_copy <- vector_landscape
+  colnames(vl_copy) = c("type", "geometry")
   vl <- get_patches(vector_landscape, "class", 4)
+  vl2 <- get_patches(vl_copy, "type", 4)
   expect_s3_class(vl, "sf")
   expect_equal(sf::st_bbox(vl), sf::st_bbox(vector_landscape))
+  expect_equal(c(colnames(vl_copy), "patch") |> sort(), colnames(vl2) |> sort())
 })
