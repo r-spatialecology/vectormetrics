7bf7cfc1541e52bdb62c02fbb1152eaa8c89d16f
Tomek <tom.mateuszek@gmail.com>
2024-02-28 15:47:40 +0100

    new unit tests

---
+++ tests/testthat/test_vm_l_square_mn.R
@@ -0,0 +1,17 @@
+testthat::test_that("check vm_l_square_mn value", {
+  expect_equal(vm_l_square_mn(square |> rbind(circle) |> rbind(diamond), "class")$value, 1.007, tolerance = 0.001)
+})
+
+testthat::test_that("check vm_l_square_mn result assertions", {
+  expect_error(vm_l_square_mn(vector_patches |> sf::st_centroid(), "class"))
+  expect_message(vm_l_square_mn(vector_landscape, "class"), "MULTIPOLYGON geometry provided")
+})
+
+testthat::test_that("check vm_l_square_mn result structure", {
+  expect_s3_class(vm_l_square_mn(square, "class"), "tbl_df")
+  expect_equal(ncol(vm_l_square_mn(square, "class")), 5)
+  expect_equal(nrow(vm_l_square_mn(vector_patches, "class")), 1)
+  expect_true(is.na(vm_l_square_mn(squaretxt, "class")$class))
+  expect_true(is.na(vm_l_square_mn(squaretxt, "class")$id))
+  expect_type(vm_l_square_mn(square, "class")$value, "double")
+})
