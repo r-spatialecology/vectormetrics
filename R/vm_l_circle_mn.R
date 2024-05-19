a24be42ab5e710cd83aa71b672cc2ccc0e9052d6
Tomek Matuszek <tom.mateuszek@gmail.com>
2024-05-19 23:12:59 +0200

    corrected docs

--- R/vm_l_circle_mn.R
+++ R/vm_l_circle_mn.R
@@ -4,13 +4,12 @@
 #' @description This function allows you to calculate the mean value of ratios of each class in a categorical landscape in vector data format
 #' the ratio is the patch area relative to area of the smallest circumscribing circle of the patch
 #' @param landscape the input landscape image,
-#' @param class_col the name of the class column of the input landscape
 #' @return  the returned calculated mean value of ratios of each class is in column "value",
 #' and this function returns also some important information such as level, class number and metric name.
 #' Moreover, the "id" column, although it is just NA here at class level. we need it because the output struture of metrics
 #' at class level should correspond to patch level one by one, and then it is more convinient to combine metric values at different levels and compare them.
 #' @examples
-#' vm_l_circle_mn(vector_landscape, "class")
+#' vm_l_circle_mn(vector_landscape)
 #' @export
 
 vm_l_circle_mn <- function(landscape){
